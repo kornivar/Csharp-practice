658a91e16c99bbffbb6f72d446ce812e84807d24
kornivar <kornivar62@gmail.com>
2026-03-04 11:13:29 +0200

    DesignPatterns: Structural: Add Facade pattern

---
+++ DesignPatterns/Structural/Facade/Program.cs
@@ -0,0 +1,58 @@
+﻿public class ColaFacade
+{
+    private readonly ColaMachine _colaMachine;
+    public ColaFacade()
+    {
+        _colaMachine = new ColaMachine();
+    }
+    public string OrderCola()
+    {
+        string result = "ColaMashine(facade) initializes subsystems:\n";
+        result += _colaMachine.isWater();
+        result += _colaMachine.isSyrup();
+
+        result += "\nColaMashine(facade) orders subsystems to perform the action:\n";
+        result += _colaMachine.FillWater();
+        result += _colaMachine.AddSyrup();
+        result += _colaMachine.Chill();
+        return result;
+    }
+}
+
+public class ColaMachine
+{
+    public string isWater()
+    {
+        return "Checking if water is available...\n";
+    }
+
+    public string isSyrup()
+    {
+        return "Checking if syrup is available...\n";
+    }
+
+    public string FillWater()
+    {
+        return "Filling water...\n";
+    }
+
+    public string AddSyrup()
+    {
+        return "Adding syrup...\n";
+    }
+
+    public string Chill()
+    {
+        return "Chilling the cola...\n";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ColaFacade colaFacade = new ColaFacade();
+        string result = colaFacade.OrderCola();
+        Console.WriteLine(result);
+    }
+}
