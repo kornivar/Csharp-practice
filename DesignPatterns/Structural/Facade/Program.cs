7e53a816dcc32f0054a4b3ce568669360230eb9e
kornivar <kornivar62@gmail.com>
2026-03-04 11:24:57 +0200

    DesignPatterns: Structural: Facade: logic rework

--- DesignPatterns/Structural/Facade/Program.cs
+++ DesignPatterns/Structural/Facade/Program.cs
@@ -1,49 +1,58 @@
-﻿public class ColaFacade
+﻿public class ColaMashine
 {
-    private readonly ColaMachine _colaMachine;
-    public ColaFacade()
+    private readonly WaterDispensing _waterDispensing;
+    private readonly SyrupDispensing _syrupDispensing;
+    private readonly Chilling _chilling;
+
+    public ColaMashine()
     {
-        _colaMachine = new ColaMachine();
+        _waterDispensing = new WaterDispensing();
+        _syrupDispensing = new SyrupDispensing();
+        _chilling = new Chilling();
     }
     public string OrderCola()
     {
-        string result = "ColaMashine(facade) initializes subsystems:\n";
-        result += _colaMachine.isWater();
-        result += _colaMachine.isSyrup();
+        string result = "ColaMashine(facade) initializes subsystem:\n";
+        result += _waterDispensing.isWater();
+        result += _syrupDispensing.isSyrup();
 
-        result += "\nColaMashine(facade) orders subsystems to perform the action:\n";
-        result += _colaMachine.FillWater();
-        result += _colaMachine.AddSyrup();
-        result += _colaMachine.Chill();
+        result += "\nColaMashine(facade) orders subsystem to perform the action:\n";
+        result += _waterDispensing.FillWater();
+        result += _syrupDispensing.AddSyrup();
+        result += _chilling.Chill();
         return result;
     }
 }
 
-public class ColaMachine
+public class WaterDispensing
 {
     public string isWater()
     {
-        return "Checking if water is available...\n";
+        return "WaterDispensing: Checking water supply...\n";
     }
-
-    public string isSyrup()
-    {
-        return "Checking if syrup is available...\n";
-    }
-
     public string FillWater()
     {
-        return "Filling water...\n";
+        return "WaterDispensing: Filling water...\n";
     }
+}
 
+public class SyrupDispensing
+{
+    public string isSyrup()
+    {
+        return "SyrupDispensing: Checking syrup supply...\n";
+    }
     public string AddSyrup()
     {
-        return "Adding syrup...\n";
+        return "SyrupDispensing: Adding syrup...\n";
     }
+}
 
+public class Chilling
+{
     public string Chill()
     {
-        return "Chilling the cola...\n";
+        return "Chilling: Chilling the cola...\n";
     }
 }
 
@@ -51,7 +60,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        ColaFacade colaFacade = new ColaFacade();
+        ColaMashine colaFacade = new ColaMashine();
         string result = colaFacade.OrderCola();
         Console.WriteLine(result);
     }
