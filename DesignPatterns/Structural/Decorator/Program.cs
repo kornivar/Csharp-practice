4a2aa816350fc4e84f43b4b87182e13e2776845e
kornivar <kornivar62@gmail.com>
2026-03-09 14:26:07 +0200

    DesignPatterns: Structural: Add Decorator

---
+++ DesignPatterns/Structural/Decorator/Program.cs
@@ -0,0 +1,53 @@
+﻿public abstract class Notifier
+{
+    public abstract void Notify(string message);
+}
+
+class ConcreteNotifier : Notifier
+{
+    public override void Notify(string message)
+    {
+        Console.WriteLine($"Notification: {message}");
+    }
+}
+
+public abstract class NotifierWrapper : Notifier
+{
+    protected readonly Notifier _notifier;
+    public NotifierWrapper(Notifier notifier)
+    {
+        _notifier = notifier;
+    }
+    public override void Notify(string message)
+    {
+        _notifier.Notify(message);
+    }
+}
+
+class EmailNotifierWrapper : NotifierWrapper
+{
+    public EmailNotifierWrapper(Notifier notifier) : base(notifier) { }
+    private void SendEmail(string message)
+    {
+        Console.WriteLine($"Sending email with message: {message}");
+    }
+    public override void Notify(string message)
+    {
+        base.Notify(message);
+        SendEmail(message);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Notifier notifier = new ConcreteNotifier();
+        notifier.Notify("Hello, World!");
+
+        Console.WriteLine("\n--Using Email Notifier Wrapper--\n");
+
+        Notifier emailNotifier = new EmailNotifierWrapper(notifier);
+        emailNotifier.Notify("Hello, World!");
+    }
+}
